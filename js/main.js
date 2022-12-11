a6420e631faa420341e772f878b590ba3e5393cf
WantToBePro31 <frederick.wijayadisusilo@gmail.com>
2022-12-11 13:48:15 +0700

    add one more figure

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const view = new View()
 view.init()
 const figure = new Figure()
 figure.init(view.scene)
+figure.head.position.x -= 2.5
+figure.head.rotation.y += 0.3
+figure.body.position.x -= 2.5
+figure.body.rotation.y += 0.3
 
 gsap.set(figure.params, {
 	y: -1.5
@@ -28,5 +32,35 @@ gsap.to(figure.params, {
 
 gsap.ticker.add(() => {
 	figure.bounce()
+	view.render()
+})
+
+const figure2 = new Figure()
+figure2.init(view.scene)
+figure2.head.position.x += 2.5
+figure2.head.rotation.y -= 0.3
+figure2.body.position.x += 2.5
+figure2.body.rotation.y -= 0.3
+
+gsap.set(figure2.params, {
+	y: -1.5
+})
+
+gsap.to(figure2.params, {
+	ry: helper.degreesToRadians(360),
+	repeat: -1,
+	duration: 20
+})
+
+gsap.to(figure2.params, {
+	y: 0,
+	armRotation: helper.degreesToRadians(90),
+	repeat: -1,
+	yoyo: true,
+	duration: 0.5
+})
+
+gsap.ticker.add(() => {
+	figure2.bounce()
 	view.render()
 })
